7fe6dbf7634c8cf9b8d0f2cbf83d48d7a6b215ef
Victoria Lee <victoriacmlee@gmail.com>
2020-09-30 05:46:12 -0400

    pseudo code

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # day-planner
+
+Project Introduction
+This is an in-browser calendar application that provides users the feature to save events for each hour of the day.
+
+Project Objective:
+Deploy an interactive web application that is fully functional and adaptive to user needs.
+
+Areas of focus:
+    jQuery programing 
+    Incorporated Third-party APIs
+    User interactive display features
+    Fully functional button events
+    Memory/storage implementation
+
+
+Project Repository Link:
+https://github.com/victoriacmlee/day-planner
+
+Project Web Link:
+https://victoriacmlee.github.io/day-planner/
+
+Project Screenshots in images folder.
---
+++ script.js
@@ -0,0 +1,30 @@
+// docuement ready, clear all element values
+$(document).ready(function () {
+
+
+    // define all targeted reference with var elements
+
+    // toRead function
+        // get time values from storage
+        // loop and assign time property as text to appropriate time
+        // display today is weekday on title
+        // display present date on page
+
+    // loop to create and format framework
+        // create row container
+        // add hour block
+        // add text area
+        // add button 
+        // add id and classes to all 
+        // check time against .now() to see if time if past, .present or .future and .assign appropriate class using < , > and =
+   
+    // saveBtnEl.on("click") call function toWrite ()
+
+    // toWrite function
+        // get value of textarea and .trim()
+        // save relevant text to local storage
+        // localStorage.setItem("textAreaVal");
+
+    // call toRead()
+
+});
